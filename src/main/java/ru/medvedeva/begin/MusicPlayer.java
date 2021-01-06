26b476eee7b863637af3fb41d36d30b829dd8740
Александра Медведева <Sa-medved@yandex.ru>
2021-01-06 13:48:42 +0700

    Теперь музыкальный проигрователь играет все песни,добавила List

--- src/main/java/ru/medvedeva/begin/MusicPlayer.java
+++ src/main/java/ru/medvedeva/begin/MusicPlayer.java
@@ -1,32 +1,21 @@
 package ru.medvedeva.begin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
-private int v;
-    //IoC
-    public MusicPlayer(Music music)
-    {
-        this.music=music;
-    }
+    private List<Music>musicList=new ArrayList<>();
     public MusicPlayer()
     {
 
     }
-
-    public int getV() {
-        return v;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void setV(int v) {
-        this.v = v;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void PlayMusic()
+    public void PlayMusicList()
     {
-        System.out.println("Playing "+music.getSong());
+        System.out.println(musicList.get(0).getSong()+" "+musicList.get(1).getSong()+" "+musicList.get(2).getSong());
+
     }
 }
--- src/main/java/ru/medvedeva/begin/TestSpring.java
+++ src/main/java/ru/medvedeva/begin/TestSpring.java
@@ -5,12 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("appContext.xml");
-       //Music music= context.getBean("musicBean",Music.class);
-      // MusicPlayer musicPlayer=new MusicPlayer(music);
-        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.PlayMusic();
 
-        System.out.println(musicPlayer.getV());
+        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer.PlayMusicList();
         context.close();
     }
 }
